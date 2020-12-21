15cbdaf82394482025e1b574f11391ae1e9c0dc2
raidentek <33987212+raidentek@users.noreply.github.com>
2020-12-20 20:25:06 -0500

    nuevo

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class',
@@ -8,6 +10,28 @@ module.exports = {
       //   sans: ["Oswald", "sans-serif"],
       // },
       colors: {
+          rose: colors.rose,
+          pink: colors.pink,
+          orange: colors.orange,
+          cyan: colors.cyan,
+          fuchsia: colors.fuchsia,
+          purple: colors.purple,
+          violet: colors.violet,
+          indigo: colors.indigo,
+          lightBlue: colors.lightBlue,
+          teal: colors.teal,
+          emerald: colors.emerald,
+          lime: colors.lime,
+          amber: colors.amber,
+          green: colors.green,
+          red: colors.red,
+  
+          warmGray: colors.warmGray,
+          trueGray: colors.trueGray,
+          coolGray: colors.coolGray,
+          blueGray: colors.blueGray,
+          gray: colors.gray,
+
         "verde-x": "#17e9d7",
 
         azul: {
